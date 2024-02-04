a8d348887e343f147f199b18dfb8f0e8681b5075
Peter Kuchel <petermichaelkuchel@gmail.com>
2024-02-04 23:06:25 +0000

    common headers and functions that can be used

---
+++ src/common.c
@@ -0,0 +1,35 @@
+#include "common.h"
+
+
+long strtol_parse(char* str){
+    char* endptr; 
+    long str_res; 
+
+
+
+    // printf("%s\n", str);
+
+    if (!strncmp(str, "0x", 2))
+        str_res = strtol(str, &endptr, 0);
+    else 
+        str_res = strtol(str, &endptr, 10);
+
+    /*check for errors*/
+    if (
+        ((errno == ERANGE) && (str_res == LONG_MAX || str_res == LONG_MIN))     // ERANGE : Result too large (POSIX.1, C99).     
+        || ( errno != 0 && str_res == 0 )                                       // something wrong if errno is non 0 and port is 0 
+    ){
+        perror("strtol"); 
+        exit(EXIT_FAILURE);
+    }
+
+    // no digits were detected at all in this case 
+    if (str == endptr){
+        fprintf(stderr, "Not a correct port\nPort needs to be between 1 - 65535");
+        exit(EXIT_FAILURE);
+    }
+
+    // printf("%ld\n", str_res);
+
+    return str_res;
+}
---
+++ src/common.h
@@ -0,0 +1,16 @@
+#ifndef CLI_COMMON_H
+#define CLI_COMMON_H
+
+#include <stdio.h> 
+#include <unistd.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <errno.h>
+#include <string.h> 
+
+
+
+long strtol_parse(char* str); 
+
+
+#endif
