468908d8b746d08d929938fb5f7270b47183a9d8
peter-kuchel <petermichaelkuchel@gmail.com>
2024-08-04 00:01:39 -0600

    adding support for \w, plus refactor

--- src/lgrep.cc
+++ src/lgrep.cc
@@ -1,25 +1,44 @@
 #include "Server.h"
 
+void create_chr_set(chr_set& c_set, const std::string& chars){
+
+    for (auto c = std::begin(chars); c != std::end(chars); ++c)
+        c_set.insert(*c); 
+    
+}
+
+bool check_exists_in(const std::string& str, chr_set& check_set){
+    for ( auto chr = std::begin(str); chr != std::end(str); ++chr ){
+            chr_itr has_digit = check_set.find(*chr);
+
+            if ( has_digit != check_set.end() ){
+                return 1; 
+            }
+        }
+        return 0;
+}
+
 bool match_pattern(const std::string& input_line, const std::string& pattern) {
     if (pattern.length() == 1) {
         return input_line.find(pattern) != std::string::npos;
 
     } else if (pattern.compare("\\d") == 0){
+        return check_exists_in(input_line, DIGITS);
+        
+    } else if (pattern.compare("\\w") == 0){
+        return check_exists_in(input_line, ALPHA_NUMERIC);
 
-        for (auto chr_itr = std::begin(input_line); chr_itr != std::end(input_line); ++chr_itr){
-            std::unordered_set<char>::const_iterator has_digit = DIGITS.find(*chr_itr);
-
-            if ( has_digit != DIGITS.end() ){
-                return 1; 
-            }
-        }
-        return 0;
     } else {
         throw std::runtime_error("Unhandled pattern " + pattern);
+        
     }
 }
 
 int main(int argc, char* argv[]) {
+
+    create_chr_set(DIGITS, CHAR_DIGITS);
+    create_chr_set(ALPHA_NUMERIC, CHAR_UPPER + CHAR_LOWER + UNDER_SCORE + CHAR_DIGITS);
+
     // Flush after every std::cout / std::cerr
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
--- src/lgrep.h
+++ src/lgrep.h
@@ -5,7 +5,18 @@
 #include <string>
 #include <unordered_set>
 
-const std::unordered_set<char> DIGITS ({'0', '1','2', '3', '4', '5', '6', '7', '8', '9'}); 
+typedef std::unordered_set<char> chr_set; 
+typedef std::unordered_set<char>::const_iterator chr_itr;
+
+void create_chr_set(chr_set& c_set, std::string& chars);
+
+const std::string CHAR_UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const std::string CHAR_LOWER = "abcdefghijklmnopqrstuvwxyz";
+const std::string UNDER_SCORE = "_";
+const std::string CHAR_DIGITS = "0123456789";
+
+chr_set DIGITS;
+chr_set ALPHA_NUMERIC;  
 
 
 #endif 
