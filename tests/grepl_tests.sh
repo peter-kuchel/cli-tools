de1b089031bb87fbfc9cd3767c6919af20f2d66f
peter-kuchel <petermichaelkuchel@gmail.com>
2024-08-22 22:07:48 -0600

    adding tests for backreferences

--- tests/grepl_tests.sh
+++ tests/grepl_tests.sh
@@ -6,6 +6,24 @@ GREPL_BIN='./bin/grepl'
 
 # Tests taken from code crafters tests for each stage
 
+# Backreferences tests 
+echo -n "cat and cat" | ./your_program.sh -E "(cat) and \1"
+echo -n "cat and dog" | ./your_program.sh -E "(cat) and \1"
+echo -n "grep 101 is doing grep 101 times" | ./your_program.sh -E "(\w\w\w\w \d\d\d) is doing \1 times"
+echo -n "$?! 101 is doing $?! 101 times" | ./your_program.sh -E "(\w\w\w \d\d\d) is doing \1 times"
+echo -n "grep yes is doing grep yes times" | ./your_program.sh -E "(\w\w\w\w \d\d\d) is doing \1 times"
+echo -n "abcd is abcd, not efg" | ./your_program.sh -E "([abcd]+) is \1, not [^xyz]+"
+echo -n "efgh is efgh, not efg" | ./your_program.sh -E "([abcd]+) is \1, not [^xyz]+"
+echo -n "abcd is abcd, not xyz" | ./your_program.sh -E "([abcd]+) is \1, not [^xyz]+"
+echo -n "this starts and ends with this" | ./your_program.sh -E "^(\w+) starts and ends with \1$"
+echo -n "that starts and ends with this" | ./your_program.sh -E "^(this) starts and ends with \1$"
+echo -n "this starts and ends with this?" | ./your_program.sh -E "^(this) starts and ends with \1$"
+echo -n "once a dreaaamer, always a dreaaamer" | ./your_program.sh -E "once a (drea+mer), alwaysz? a \1"
+echo -n "once a dremer, always a dreaaamer" | ./your_program.sh -E "once a (drea+mer), alwaysz? a \1"
+echo -n "once a dreaaamer, alwayszzz a dreaaamer" | ./your_program.sh -E "once a (drea+mer), alwaysz? a \1"
+echo -n "bugs here and bugs there" | ./your_program.sh -E "(b..s|c..e) here and \1 there"
+echo -n "bugz here and bugs there" | ./your_program.sh -E "(b..s|c..e) here and \1 there"
+
 # Alternation tests 
 echo -n "a cat" | $GREPL_BIN -E "a (cat|dog)"
 echo -n "a dog" | $GREPL_BIN -E "a (cat|dog)"
@@ -63,6 +81,3 @@ echo -n "apple" | $GREPL_BIN -E "\d"
 # Match character literal 
 echo -n "dog" | $GREPL_BIN -E "d"
 echo -n "dog" | $GREPL_BIN -E "f"
-
-
-
